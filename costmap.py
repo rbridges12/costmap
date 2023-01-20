be759f9e055752a7c58775370d53e28b8888cada
Riley Bridges <rlybrdgs@gmail.com>
2023-01-20 02:31:02 -0500

    layed out high level functions

--- costmap.py
+++ costmap.py
@@ -4,10 +4,16 @@ CM_CELL_SIZE = 1
 CM_WIDTH = 10
 
 class CostMap:
+    # transforms
     map_to_corner: np.ndarray
     corner_to_cm: np.ndarray
     cm_to_rover: np.ndarray
+
+    # NxN grid to store cost values
     cm_grid: np.ndarray
+
+    # 2XN array of points underlying grid
+    cm_points: np.ndarray
 
     def __init__(self) -> None:
         
@@ -20,10 +26,13 @@ class CostMap:
         self.corner_to_cm[:2, 2] = [CM_WIDTH / 2, CM_WIDTH / 2]
 
         # determine costmap size from width and cell size, init to zeros
-        cells = np.ceil(CM_WIDTH / CM_CELL_SIZE).astype(int)
-        self.cm_grid = np.zeros((cells, cells))
+        self.num_cells = np.ceil(CM_WIDTH / CM_CELL_SIZE).astype(int)
+        self.cm_grid = np.zeros((self.num_cells, self.num_cells))
+        
+        # TODO: init with nans?
+        self.cm_points = np.zeros((self.num_cells, self.num_cells, 2))
     
-    def new_pointcloud(self, points: np.ndarray):
+    def add_new_points(self, points: np.ndarray):
         """
         update the costmap given the new set of points
         :param points: 2xN numpy array containing [x, y] points in the rover frame
@@ -38,12 +47,25 @@ class CostMap:
         # convert out of homogenous form
         points_corner = h_points_corner[:2, :]
 
-    def update_costmap(self, points: np.ndarray):
-        """
-        add the given set of points to the costmap
-        :param points: 2xN numpy array containing [x, y] points in the costmap corner frame
-        """
+        # get costmap indices to put points at
+        ids = np.floor(points / CM_CELL_SIZE).astype(int)
+        valid_mask = np.any(ids < self.num_cells, axis=0) 
+        valid_ids = ids[:, valid_mask]
+        self.cm_points 
 
-        for p in points.T:
-            x_id = np.floor((p[0] - min_x) / pixel_size).astype(int)
-            y_id = np.floor((p[1] - min_y) / pixel_size).astype(int)
+    def update_rover(self, map_to_rover: np.ndarray):
+        
+        # calculate delta transform
+        
+        # propagate costmap with delta transform
+        
+        # split map to rover transform into map to cm and cm to rover, update each
+    
+    def propagate(self, delta_tf: np.ndarray):
+        
+        # apply delta_tf to all underlying points
+
+        
+        # empty costmap
+        
+        # add_new_points to costmap
---
+++ visualize.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_costmap(map_to_cm: np.ndarray, costmap: np.ndarray):
+    """
+    Plot costmap as an image in matplotlib, assumes cell width is 1.
+    # TODO: use pygame
+    """
+    
+
+def visualize()
